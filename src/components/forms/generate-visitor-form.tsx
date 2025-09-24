d63d4ae5c89fb9b6cad98ce35a2b1ea3f3b4f352
Wael Yacoub <wael.yacoub@gmail.com>
2025-09-24 14:21:11 +0000

    remove date pick up for visitor pass type and make it valid until only t

--- src/components/forms/generate-visitor-form.tsx
+++ src/components/forms/generate-visitor-form.tsx
@@ -206,45 +206,7 @@ export default function GenerateVisitorForm() {
                 </FormItem>
             )}
           />
-          <FormField
-            control={form.control}
-            name="expiresAt"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Valid Until</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      disabled={(date) => date < new Date()}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+
           <Button type="submit" disabled={isSubmitting || userLoading} className="w-full">
             {isSubmitting && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
             Create Pass
